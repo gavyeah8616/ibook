7f4048efd12de882bcf1f75dc9aee871be526393
GavYeah <161093496+gavyeah8616@users.noreply.github.com>
2024-06-20 20:40:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ app = Flask(__name__, template_folder='', static_folder='')
 def helloworld():
 #  return "<p>Hello!</p>"
   return render_template("index.html")
+@app.route("/about")
+def about():
+  return render_template("about.html")
 if __name__ == '__main__' :
   app.run(host='0.0.0.0', port=5000)
