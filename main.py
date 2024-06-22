1b4eaf89894c605f782154ab7fdccbf30af09a35
GavYeah <161093496+gavyeah8616@users.noreply.github.com>
2024-06-21 20:21:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ app = Flask(__name__, template_folder='', static_folder='')
 
 @app.route('/')
 def helloworld():
-#  return "<p>Hello!</p>"
   return render_template("index.html")
+  
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/how-to-use-ibooks")
+def howtoibook():
+    return "<p>Hello! u may be wondering how to use ibooks. well it's easy</p>"
+  
+
 if __name__ == '__main__' :
   app.run(host='0.0.0.0', port=5000)
